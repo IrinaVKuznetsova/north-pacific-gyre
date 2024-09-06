ec0bb26bcba27c589c3599359c680035ba071371
IK <irina.kuznetsova@curtin.edu.au>
2024-09-06 03:50:31 +0000

    Add welcome message and collect filename

--- goostats.sh
+++ goostats.sh
@@ -1,3 +1,6 @@
 # Load a given file
 # Compute the min/max/range of values in a file
-
+frame=$1
+echo "Working with file:"
+echo "Welcome to Nelle's stats script"
+# Compute the min/max/range of values in a file
