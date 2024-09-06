552449ae40cd7412454f7c12e7e7e1aa00470e8e
IK <irina.kuznetsova@curtin.edu.au>
2024-09-06 03:57:04 +0000

    use supplied filename

--- goostats.sh
+++ goostats.sh
@@ -1,6 +1,6 @@
 # Load a given file
 # Compute the min/max/range of values in a file
-frame=$1
-echo "Working with file:"
+fname=$1
+echo "Working with file: ${fname}"
 echo "Welcome to Nelle's stats script"
 # Compute the min/max/range of values in a file
